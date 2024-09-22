76dce0c45c402a91985e8b05b9bd03bca7135683
saloni aggarwal <112769048+SALONI-AGGARWAL0304@users.noreply.github.com>
2024-09-22 12:38:33 +0530

    improvements

--- server.js
+++ server.js
@@ -22,18 +22,18 @@ app.get("/", function (req, resp) {
   let path = process.cwd() + "/public/index.html";
   resp.sendFile(path);
 });
-const configobj = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database:  process.env.DB_DBNAME,
-};
 // const configobj = {
-//   host: "127.0.0.1",
-//   user: "root",
-//   password: "Saloni##2004",
-//   database: "project",
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database:  process.env.DB_DBNAME,
 // };
+const configobj = {
+  host: "127.0.0.1",
+  user: "root",
+  password: "Saloni##2004",
+  database: "project",
+};
 const mysql = mysql2.createConnection(configobj);
 mysql.connect(function (err) {
   if (err == null) {
@@ -184,7 +184,7 @@ app.get("/customer_manager", function (req, resp) {
   resp.sendFile(filepath);
 });
 app.get("/customer-task", function (req, resp) {
-  mysql.query("select *from tasks", function (err, resultcustomer) {
+  mysql.query("select *from tasks where emailid=?",[req.query.mailID], function (err, resultcustomer) {
     if (err == null) {
       resp.send(resultcustomer);
     } else {
